fe7330b9fa4cd6bd922e3357b3a448352d9a0b32
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2019-04-28 12:06:44 +0100

    minor

--- core/src/test/scala/com/softwaremill/ratelimiter/RateLimiterQueueSpec.scala
+++ core/src/test/scala/com/softwaremill/ratelimiter/RateLimiterQueueSpec.scala
@@ -11,20 +11,24 @@ import scalaz.zio.IO
 class RateLimiterQueueSpec extends WordSpec with Matchers {
 
   "RateLimiterQueue" should {
-    "create a RateLimiterQueue using companion object" in {
-      val actal: RateLimiterQueue[Nothing] = RateLimiterQueue(1, 1000)
 
-      actal shouldBe RateLimiterQueue(1, 1000, Queue(), Queue(), scheduled = false)
+    "create a RateLimiterQueue using companion object" in {
+      val actual: RateLimiterQueue[Nothing] = RateLimiterQueue(1, 1000)
+
+      actual shouldBe RateLimiterQueue(1, 1000, Queue(), Queue(), scheduled = false)
     }
+
     "run" in {
       val io1 = IO.sync(println("task1"))
       val io2 = IO.sync(println("task2"))
       val io3 = IO.sync(println("task3"))
+      val perMillis = 1000
+      val three = List(io1, io2, io3).foldLeft[RateLimiterQueue[IO[Nothing, Unit]]](RateLimiterQueue(maxRuns = 2, perMillis))((acc, io) => acc.enqueue(io))
+      val startTime = System.currentTimeMillis()
 
-      val three = List(io1, io2, io3).foldLeft[RateLimiterQueue[IO[Nothing, Unit]]](RateLimiterQueue(2, 1000))((acc, io) => acc.enqueue(io))
+      val (tasks, queue) = three.run(startTime)
 
-      val (tasks, queue) = three.run(System.currentTimeMillis())
-      tasks shouldBe List(Run(io1), Run(io2), RunAfter(1000))
+      tasks shouldBe List(Run(io1), Run(io2), RunAfter(perMillis))
       println(tasks)
       println(queue)
     }
